0ef631580e72c57d430cba2b7fc16ba8c7ede775
Jovelyn Margallo <jovelyn.r.margallo@accenture.com>
2023-03-22 14:35:04 +0000

    Initial Commit - Case Study

---
+++ employeeapp/webapp/controller/EmployeeList.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("sapips.training.employeeapp.controller.EmployeeList", {
+            onInit: function () {
+
+            },
+            onListItemPress: function(oEvent){
+                
+            }
+        });
+    });
